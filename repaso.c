6729420774b5454d22e5ea9d6597fe0897fb7edb
billgiberti <maximiliano.gilberto@gmail.com>
2023-10-11 23:10:56 -0300

    repaso-pt-struct

--- repaso.c
+++ repaso.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
 
 //compilar c: gcc nombrefile -o ejecutable
 //compilar cpp: g++ nombrefile -o ejecutable
 struct persona{
-   char nombre[]="maximiliano";
-   char apellido[]="gilberto";
+   char nombre[30];
+   char apellido[30];
    int edad;
 };
 
 int main()
 {
 	
-	struct persona p1;
+	struct persona *p1;
 	
-	//p1.apellido[] = {'M', 'a', 'x', 'i', 'm', 'i', 'l', 'i', 'a', 'n', 'o', '\0'};
-	p1.edad= 38;
+	p1=(struct persona *)malloc(sizeof(struct persona));
+	strcpy(p1->nombre,"maximiliano");
+	strcpy(p1->apellido,"gilberto");
+	p1->edad = 38;
 	
 
 	
-	printf("%s\n",p1.nombre);
-	printf("%s\n",p1.apellido);
-	printf("%i\n",p1.edad);
+	printf("%s\n",p1->nombre);
+	printf("%s\n",p1->apellido);
+	printf("%i\n",p1->edad);
 	return 0;
 }
