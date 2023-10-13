3b138d3aca2d6ac44061a938a06a54361faed2c8
billgiberti <maximiliano.gilberto@gmail.com>
2023-10-13 09:37:14 -0300

    fin-repaso

--- repaso.c
+++ repaso.c
@@ -15,15 +15,20 @@ int main()
 	
 	struct persona *p1;
 	
-	p1=(struct persona *)malloc(sizeof(struct persona));
+	p1=(struct persona *)malloc(2*sizeof(struct persona));
 	strcpy(p1->nombre,"maximiliano");
 	strcpy(p1->apellido,"gilberto");
 	p1->edad = 38;
-	
+	strcpy(p1[1].nombre,"faiana");
+        strcpy(p1[1].apellido,"abadie");
+        p1[1].edad = 42;
 
 	
-	printf("%s\n",p1->nombre);
-	printf("%s\n",p1->apellido);
-	printf("%i\n",p1->edad);
+	printf("%s\n",p1[0].nombre);
+	printf("%s\n",p1[0].apellido);
+	printf("%i\n",p1[0].edad);
+	printf("%s\n",p1[1].nombre);
+        printf("%s\n",p1[1].apellido);
+        printf("%i\n",p1[1].edad);
 	return 0;
 }
